e262fdeb780508e193627a63698434681e30c316
Camilla Belponer <camilla.belponer@gmail.com>
2020-03-18 19:18:28 +0100

    aggiunti file gpu

---
+++ include/LocalSearchGPU.hpp
@@ -0,0 +1,52 @@
+#ifndef LOCAL_SEARCH_GPU_HH
+#define LOCAL_SEARCH_GPU_HH
+
+#include "LocalSearch.hpp"
+
+class LocalSearchGPU : public LocalSearch
+{
+
+public:
+
+    // map a maxGPU in un setup
+    typedef std::unordered_multimap<unsigned, Setup> boh_t;
+
+    // neighboorhood type: maps the index of a node into a possible setups
+    typedef std::unordered_multimap<unsigned, Setup> neighborhood_t;
+
+    // associates to every node the set of jobs in execution on that specific node
+    typedef std::unordered_map<unsigned, std::unordered_set<Job>> node_jobs_t;
+
+private:
+
+    //
+    boh_t all_neighborhoods; //TODO: costruirla
+
+    // pairings node_idx - jobs on that node (it should be updated every time initial_schedule changes)
+    node_jobs_t node_jobs;
+
+    // visit the neighborhood of initial schedule
+    bool visit_neighbor(void) override;
+
+    // generates the neighboorhood to explore
+    neighborhood_t generate_neighborhood(void);
+
+    // changes the schedule according to node and GPU_type
+    job_schedule_t change_GPU(unsigned, std::string);
+
+    //initialize the map GPU-VM starting from ttime (ttime is a protected memeber of the parent class Heuristic)
+    void initialize_all_neighborhoods(void);
+
+    // update the set node_jobs starting from initial_schedule
+    void update_node_jobs(void);
+
+    // find the indexes of top nodes to change in the neighbourhood
+    std::set<unsigned> FindTopNodes(unsigned);
+
+public:
+
+    LocalSearchGPU (const std::string&, const std::string&, const std::string&, const std::string&, const std::string&);
+
+};
+
+#endif /* LOCAL_SEARCH_GPU_HH */
